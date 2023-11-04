56f7234ed22178bd6f44cae12fd7ae5358d6e16d
claudimes <143952529+claudimes@users.noreply.github.com>
2023-11-04 20:05:46 -0300

    04-11-23

---
+++ exe301.py
@@ -0,0 +1,33 @@
+class Curso():
+    def __int__(self):
+        self.nome = " "
+
+    def getNome(self):
+        return self.nome
+
+    def setNome(self, nome):
+        self.nome = nome
+
+class Aluno():
+    def __init__(self):
+        self.curso = None
+
+    def getCurso(self):
+        return self.curso
+
+    def setCurso(self,curso):
+        self.curso = curso
+
+    def getNomeCurso(self):
+        if self.curso == None:
+            return "Aluno sem Curso"
+        else:
+            return self.curso.getNome()
+
+curso = Curso()
+curso.setNome('Engenharia de Software')
+
+aluno = Aluno()
+#aluno.setCurso(curso)
+
+print(aluno.getNomeCurso())
